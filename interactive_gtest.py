04c820db992a610152e98d1d3a4a0c750dd8159d
nannatk <nanna.tks@gmail.com>
2024-04-21 07:55:50 +0900

    first impl

--- interactive_gtest.py
+++ interactive_gtest.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 import subprocess
 import re
+import argparse
 
 
 class SimpleMenu(object):
     def __init__(self):
         pass
 
-    def select(self, candidates):
+    def select(self, candidates, title=None):
         idx_width = len(str(len(candidates) - 1))
-        #idx_format = ':>{idx_width}'
+        menu_width = 50
+        print('')
+        if(title):
+            titlestr = ('=' * 15) + ' {} '.format(title) + ('=' * 15)
+            print(titlestr)
+            menu_width = len(titlestr)
+
         for i, candidate in enumerate(candidates):
             print('[{:>{idx_width}}]: {}'.format(i, candidate, idx_width=idx_width))
+        print('-' * menu_width)
         print('select: ', end='')
         inputstr = input()
         m = re.match(r'(?P<idx>\d+)', inputstr)
@@ -41,34 +49,36 @@ class InteractiveGtest(object):
                 while(True):
                     # Testnameの選択
                     selected_testname = self.__select_testname(menu, selected_testcase)
-                    if(selected_testcase is None):
+                    if(selected_testname is None):
                         break 
                     else:
                         self.__execute_selected_test("{}.{}".format(selected_testcase, selected_testname))
 
     
     def __select_testcase(self, menu):
-        testcases = list(self._testlist.keys())
-        if(len(testcases) == 0):
-            # Testcaseが無いことは多分無いと思うが、これは選択されなかった扱いにする
-            return None
-        elif(len(testcases) == 1):
-            # Testcaseが1つだった場合、選択済みとする
-            return testcases[0]
-        else:
-            # 先頭に全部実行の'*'を追加する
-            testcases = ['*'] + testcases
-            selected = menu.select(testcases)
-            selected = testcases[selected] if(selected) else None
-            return selected
+        # 先頭に全部実行の'* 'を追加する
+        testcases = ['*'] + list(self._testlist.keys())
+        while(True):
+            selected = menu.select(testcases, title='select testcase')
+            if(selected is None):
+                return None 
+            else:
+                # 範囲外Idxだった場合、再度メニュー表示する
+                if(selected < len(testcases)):
+                    return testcases[selected]
 
     def __select_testname(self, menu, testcase):
         # Testcaseと違い、Testnameは中身が1つだろうと0だろうとMenuを表示する
         # 先頭に全部実行の'*'を追加する
         testnames = ['*'] + list(self._testlist[testcase])
-        selected = menu.select(testnames)
-        selected = testnames[selected] if(selected) else None
-        return selected
+        while(True):
+            selected = menu.select(testnames, title='select testname')
+            if(selected is None):
+                return None
+            else:
+                # 範囲外Idxだった場合、再度メニュー表示する
+                if(selected < len(testnames)):
+                    return testnames[selected] 
 
     def __execute_selected_test(self, selected_test):
         options = ['--gtest_filter={}'.format(selected_test)]
@@ -95,7 +105,7 @@ class InteractiveGtest(object):
         return testlist
 
     def __execute_gtest(self, options, disp=True):
-        cmd = [self._gtest] + options
+        cmd = [self._gtest, '--gtest_color=yes'] + options
         output = []
         process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
         for line in process.stdout:
@@ -105,7 +115,11 @@ class InteractiveGtest(object):
         returncode = process.wait()
         return output, returncode
 
+def get_argparse():
+    parser = argparse.ArgumentParser(description='This is an interactive, simple and quick test selector for GoogleTest.') 
+    parser.add_argument('gtest', help='Executable file path of GoogleTest.')
+    return parser
 
 if __name__ == "__main__":
-    gtest = './sample/sample1_unittest'
-    InteractiveGtest(gtest).start()
+    args = get_argparse().parse_args()
+    InteractiveGtest(args.gtest).start()
